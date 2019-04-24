04724400bd1530936618870299e509be1c1f420c
Dion Niazi <dion1008@d-172-25-143-149.dhcp.virginia.edu>
2019-04-23 22:33:40 -0400

    Fixed Classes

--- congo/classes/classes.py
+++ congo/classes/classes.py
@@ -11,3 +11,38 @@ class Merchant:
 	def __init__(self, name):
 		self.name = name
 		# TODO: put list of item IDs seller sells here.
+
+class Creditcard:
+	def __init__(self, cc_number, cc_name, cc_expDate, cc_secCode):
+		self.cc_number = cc_number
+		self.cc_name = cc_name
+		self.cc_expDate = cc_expDate
+		self.cc_secCode = cc_secCode
+
+class Customer:
+	def __init__(self, name, dob, addr, cc_number):
+		self.name = name
+		self.dob = dob
+		self.addr = addr
+		self.cc_number = cc_number
+
+class Orders:
+	def __init__(self, name, dob, addr, cc_number):
+		self.name = name
+		self.dob = dob
+		self.addr = addr
+		self.cc_number = cc_number
+
+class Reviews:
+	def __init__(self, id, data, ts, rating, merch_id):
+		self.id = id
+		self.data =
+		self.ts = ts
+		self.rating = rating
+		self.merch_id = merch_id
+
+class Sells:
+	def __init__(self, id, merch_id, m):
+		self.id = id
+		self.merch_id = merch_id
+		self.m = m

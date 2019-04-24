d5ef1ea360c716ca05fe6d48d5534f2fcb21200f
hamley241 <gpatley@gmail.com>
2019-04-23 22:34:27 -0400

    [Routes] Testing basic Ping API

--- congo/classes/classes.py
+++ congo/classes/classes.py
@@ -3,9 +3,10 @@ class Merch:
 	def __init__(self, name, price, desc, num_avail, img):
 		self.name = name
 		self.price=price
-		self.image=image
+		self.image=img
 		self.num_avail = num_avail
 		self.desc = desc
+
 
 class Merchant:
 	def __init__(self, name):
